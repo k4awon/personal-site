c21bfa9b9c62af0689ac9f899e03cd73ab564a42
Moises Sanchez <moijsanchez@gmail.com>
2021-12-29 08:36:16 +0000

    Added basic template for projects section

---
+++ src/components/webProject.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import * as data from '../data/webProject.json';
+
+let projectData = data;
+const WebProject = () => {
+    return(
+        <div id="webProjects">
+                {projectData.projects.map(project =>(
+                  <div className="text-center">
+                      <div className="projectContainer">
+                          <div>
+                              {project.name}
+                          </div>
+                          <div className="">
+                              {project.description}
+                          </div>
+                      </div>
+                  </div>
+                ))}
+        </div>
+    )
+}
+
+export default WebProject;
